70f3d4254ff90ca24c5a5f44b1ccd9b91960c92e
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2021-05-14 18:43:13 +0100

    Swagger Docs generated

--- cranch/settings.py
+++ cranch/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
 
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- cranch/urls.py
+++ cranch/urls.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
     TokenVerifyView
 )
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Cranch API",
+      default_version='v1',
+      description="Cranch API description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="pro.ajibolaojo@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui' ),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
