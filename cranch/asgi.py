c4317f2f308cf9210aa011458b4f2c58a28fa3c5
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2021-05-16 00:17:41 +0100

    asgi application setup

--- cranch/asgi.py
+++ cranch/asgi.py
@@ -1,7 +1,16 @@
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cranch.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            [] # TODO: replace with routing.websocket_urlpatterns
+        )
+    ),
+})
