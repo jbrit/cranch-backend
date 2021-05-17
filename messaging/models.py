d0f7630f4f755f5d183499580611ef47e2e58c89
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2021-05-17 18:12:17 +0100

    direct messaging model created

--- messaging/models.py
+++ messaging/models.py
@@ -1,1 +1,9 @@
+import uuid
 from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+class DirectMessaging(models.Model):
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+    members = models.ManyToManyField(User, validators=[])
