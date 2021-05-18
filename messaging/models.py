1ebe7210a1e3ec95992adfa08f00aa845095b53f
pro.ajibolaojo@gmail.com <pro.ajibolaojo@gmail.com>
2021-05-18 17:22:12 +0100

    direct message validations added

--- messaging/models.py
+++ messaging/models.py
@@ -1,8 +1,19 @@
 import uuid
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 class DirectMessaging(models.Model):
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     members = models.ManyToManyField(User)
+
+    @classmethod
+    def combination_exists(cls, members):
+        return cls.objects.filter(members=members).exists()
+
+    def clean(self, *args, **kwargs):
+        if len(self.members) != 2:
+            raise ValidationError("Exactly two members must be present in a direct message")
+        if self.combination_exists(self.members):
+            raise ValidationError("This direct message already exists")
